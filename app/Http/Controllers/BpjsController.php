600a00bbdf14aba9e15690d4e0222a3a26041933
Faishol Shinryuhachi <fahmifaishol11@gmail.com>
2018-10-19 22:54:56 +0700

    perbaikan

--- app/Http/Controllers/BpjsController.php
+++ app/Http/Controllers/BpjsController.php
@@ -14,7 +14,7 @@ class BpjsController extends Controller
 	    //echo $coba->poli("INT");
 	    //echo $coba->faskes("00161001", 1);
 	    //echo $coba->dokterDPJP(1, "2018-01-01", "31486");
-	    //echo $coba->provinsi();
+	    echo $coba->provinsi();
 	   	//echo $coba->kabupaten('13');
 	    //echo $coba->kecamatan('0177');
 	    //echo $coba->prosedur("21.05");
@@ -28,13 +28,10 @@ class BpjsController extends Controller
 	    //echo $coba->getPesertaNoBpjs($noBpjs, $tglSep);
 	    //echo $coba->getPesertaNik($nik, $tglSep);
 
-	    /*echo $coba->Insert SEP
-					public function insertSep(
-				 		$noKartu, $tglSep, $ppkPelayanan, $jnsPelayanan, $klsRawat, $noMR,
-				 		$asalRujukan, $tglRujukan, $noRujukan, $ppkRujukan, $catatan, $diagAwal, $tujuan, $eksekutif, $cob,
-				 		$katarak, $lakaLantas, $penjamin, $tglKejadian, $keterangan, $suplesi, $noSepSuplesi, $kdPropinsi,
-				 		$kdKabupaten, $kdKecamatan, $noSurat, $kodeDPJP, $noTelp, $user);
-		*/
+	    /*echo $coba->insertSep("0001112230666","2017-10-18","0301R001","2","3","123456","1","2017-10-17",
+	    	"1234567","00010001","test","A00.1","INT","0","0","0","0","1","2018-08-06","kll","0",
+	    	"0301R0010718V000001","03","0050","0574","000002","31661","081919999","Coba Ws");*/
+		
 		/*echo $coba->updateSep(
 				 		$noSep, $klsRawat, $noMR, $catatan, $diagAwal, $eksekutif, $cob, $katarak,
 				 		$noSurat, $kodeDPJP, $lakaLantas, $penjamin, $tglKejadian, $keterangan, $suplesi, $noSepSuplesi,
@@ -44,10 +41,10 @@ class BpjsController extends Controller
 		//echo $coba->cariSep($noSep);
 
 		//echo $coba->getSuplesiJasaRaharja($noKartu, $tglSep);
-		//echo $coba->pengajuan(
-			    	$noKartu, $tglSep, $jnsPelayanan, $keterangan, $user);
-		//echo $coba->aprovalPengajuan(
-			    	$noKartu, $tglSep, $jnsPelayanan, $keterangan, $user);
+		/*echo $coba->pengajuan(
+			    	$noKartu, $tglSep, $jnsPelayanan, $keterangan, $user);*/
+		/*echo $coba->aprovalPengajuan(
+			    	$noKartu, $tglSep, $jnsPelayanan, $keterangan, $user);*/
 		//echo $coba->updateTanggalPulang($noSep, $tglPulang, $ppkPelayanan)
 
 	    /*
